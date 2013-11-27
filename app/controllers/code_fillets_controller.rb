02245466e5a9fcce3dfbbd4894657725d3cb28a6
Pav <pavlifestyle@gmail.com>
2013-11-27 09:49:05 +0100

    authentication changes

--- app/controllers/code_fillets_controller.rb
+++ app/controllers/code_fillets_controller.rb
@@ -1,5 +1,7 @@
 class CodeFilletsController < ApplicationController
-  
+  before_action :authenticate_user! , :only =>[:new]
+
+
   # GET /code_fillets/
   # w przyszlosci GET /code_fillets/?tag=rails => Tag.find(params[:tag]).code_fillets
   def index
@@ -16,4 +18,24 @@ class CodeFilletsController < ApplicationController
   def new
     @code_fillet = CodeFillet.new
   end
+
+  def create
+    permitted_params = params.require(:code_fillet).permit(:name, :description)
+    @code_fillet = CodeFillet.new permitted_params
+    @code_fillet.save 
+    redirect_to @code_fillet
+  end
+
+  def edit
+    @code_fillet = CodeFillet.find params[:id]
+  end
+
+def update
+  @code_fillet = CodeFillet.find params[:id]
+  permitted_params = params.require(:code_fillet).permit(:name, :description)
+  @code_fillet.update_attributes permitted_params
+  @code_fillet.save
+  redirect_to @code_fillet
+end
+
 end
