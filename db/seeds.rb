d75a84cdd02c228faa29c524f072c684621de863
Pav <pavlifestyle@gmail.com>
2013-11-28 12:20:50 +0100

    added seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-Admin.create(email: "admin@gmail.com",password:"password",password_confirmation:"password")
+Admin.destroy_all
+User.destroy_all
+Tag.destroy_all
+Admin.create(email: "pavlifestyle@gmail.com",password:"password",password_confirmation:"password")
+User.create(username: "user",email: "user@gmail.com",password:"password",password_confirmation:"password")
+Tag.create(name: "c#")
+Tag.create(name: "ruby")
+Tag.create(name: "rvm")
+Tag.create(name: "c++")
+Tag.create(name: "objective c")
+Tag.create(name: "ruby on rails")
+Tag.create(name: "html")
+Tag.create(name: "python")
+Tag.create(name: "javascript")
+Tag.create(name: "scala")
+Tag.create(name: "visual basic")
+Tag.create(name: "extreme programming")
+Tag.create(name: "agile")
+Tag.create(name: "scrum")
